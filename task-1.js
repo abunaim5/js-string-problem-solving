35954379e7ef93b6e6986e16f09d76924de5c4b1
abunaim5 <abunaim140@gmail.com>
2024-02-01 18:36:49 +0600

    count how many times a string has the letter a

---
+++ task-1.js
@@ -0,0 +1,26 @@
+// Count how many times a string has the letter a
+const myPlace = 'I live in Bangladesh.';
+const char = 'a';
+let charSum = 0;
+for (let i = 0; i < myPlace.length; i++) {
+    // console.log(myPlace[i]);
+    if (myPlace[i] === char) {
+        charSum = charSum + 1;
+    }
+}
+console.log(aSum);
+
+// Count with function and for loop
+const str = 'I studied at Jatiya Kabi Kazi Nazrul Islam University';
+const charCount = 'a';
+function countOccurrences(str, charCount) {
+    let sumChar = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === charCount) {
+            sumChar += 1;
+        }
+    }
+    return sumChar;
+}
+const occurrenceCount = countOccurrences(str, charCount);
+console.log(occurrenceCount);
